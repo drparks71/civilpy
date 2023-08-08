6dc7eb030ab854bcbf43267c93f8919aecfb5e7e
dane <dane@daneparks.com>
2023-08-08 00:41:15 -0400

    Finished steel tools, release 0.0.46

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='civilpy',
-    version='0.0.45',
+    version='0.0.46',
     packages=find_packages('src', exclude=['test', 'secrets', 'docs', 'res']),
     description='Civil Engineering Tools in Python',
     url="https://daneparks.com/Dane/civilpy",
