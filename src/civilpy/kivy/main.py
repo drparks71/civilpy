ee8429bb597a133be5eb032f2e90cf8ab0a2605e
dane <dane@daneparks.com>
2023-08-10 22:44:48 -0400

    Testing PyQt5 as alternative to Kivy, got basic "Hello World" App running

--- src/civilpy/kivy/main.py
+++ src/civilpy/kivy/main.py
@@ -1,46 +1,1 @@
-# importing all necessary modules
-# like MDApp, MDLabel Screen, MDTextField
-# and MDRectangleFlatButton
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
-from kivymd.uix.screen import Screen
-from kivymd.uix.textfield import MDTextField
-from kivymd.uix.button import MDRectangleFlatButton
-
-
-# creating Demo Class(base class)
-class Demo(MDApp):
-
-    def build(self):
-        screen = Screen()
-
-        # defining label with all the parameters
-        l = MDLabel(text="HI PEOPLE!", halign='center',
-                    theme_text_color="Custom",
-                    text_color=(0.5, 0, 0.5, 1),
-                    font_style='Caption')
-
-        # defining Text field with all the parameters
-        name = MDTextField(text="Enter name", pos_hint={
-            'center_x': 0.8, 'center_y': 0.8},
-                           size_hint_x=None, width=100)
-
-        # defining Button with all the parameters
-        btn = MDRectangleFlatButton(text="Submit", pos_hint={
-            'center_x': 0.5, 'center_y': 0.3},
-                                    on_release=self.btnfunc)
-        # adding widgets to screen
-        screen.add_widget(name)
-        screen.add_widget(btn)
-        screen.add_widget(l)
-        # returning the screen
-        return screen
-
-    # defining a btnfun() for the button to
-    # call when clicked on it
-    def btnfunc(self, obj):
-        print("button is pressed!!")
-
-
-if __name__ == "__main__":
-    Demo().run()
+from kivy.lang import Builderfrom kivymd.app import MDAppfrom kivy.uix.camera import CameraKV = '''BoxLayout:    orientation: 'vertical'    MDTopAppBar:        title: "Camera Access with KivyMD"        md_bg_color: app.theme_cls.primary_color        elevation: 10    Camera:        id: camera        resolution: (640, 480)        play: True    MDFloatingActionButton:        icon: "camera"        md_bg_color: app.theme_cls.primary_color        pos_hint: {"center_x": 0.5}        on_press: camera.play = not camera.play  # Toggle play attribute to start/stop camera'''class MainApp(MDApp):    def build(self):        return Builder.load_string(KV)if __name__ == "__main__":    MainApp().run()
