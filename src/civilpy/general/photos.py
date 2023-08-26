4b5404a68e645c6cfd4f6d1cd231cceaa06755e7
dane <dane@daneparks.com>
2023-08-26 16:30:41 -0400

    release 0.0.52, pushed to get clean pipeline

--- src/civilpy/general/photos.py
+++ src/civilpy/general/photos.py
@@ -1,9 +1,20 @@
 import os
 import re
-from pathlib import Path
+import sys
+import shutil
+import unicodedata
+import pandas as pd
 from PIL import Image
+from numpy import unique
+from pathlib import Path
 from PIL.Image import Exif
+from collections import Counter
 from PIL.ExifTags import TAGS, GPSTAGS
+
+
+# Define these variables for use in later functions
+testing_excel_file = ''
+testing_root_folder = ''
 
 
 class CivImageMap(Image.Image):
@@ -70,8 +81,186 @@ class CivImageMap(Image.Image):
 
                 for file_var in os.listdir(directory):
                     root_dir = Path(f"{root}")
-                    print(f"Saving: {Path(root / directory / file_var)}")
+                    print(f"Saving: {Path(root) / directory / file_var}")
 
                     empty_dict[file_var]['path'] = f"{Path(root / directory / file_var)}"
 
         self.file_dict = empty_dict
+
+
+def get_list_of_files_from_folder(
+        path=r"C:\Users\dane\Desktop\test_photos"
+):
+    """
+    Builds a list of files given a root folder, can be given a folder with multiple sub-folders
+
+    Parameters
+    ----------
+    path: str - A string representation of a file path, for windows paths, either has to be passed in as a raw string,
+        or with escaped/converted '\' characters. Once inside the function, the string is converted using the pathlib
+        Path() module, eliminating further issues with windows paths.
+
+    Returns
+    -------
+    return_list: list - A full list of every file found by the function
+    """
+    return_list = []
+
+    # Build a list of every file in the given folder
+    for root, dirs, files in os.walk(Path(path).resolve()):
+        # Get all the files in the root folder
+        for file in files:
+            return_list.append(f"{Path(root) / file}")
+
+    return return_list
+
+
+def get_photos_from_file_list(file_list=None):
+    """
+    Takes a list of file paths and uses regex to only return files with photo related file types (svg, heif, bmp, tiff,
+    webp, jpeg, png, jpg). Not case-sensitive.
+
+    Parameters
+    ----------
+    file_list: list - a list of files to be filtered for Photos
+
+    Returns
+    -------
+    photos: list - a filtered list containing only the paths from the original list that are photos
+    """
+
+    photos = [file for file in file_list if re.search("(svg|heif|bmp|tiff|webp|jpeg|png|jpg)$",
+                                                      str(file), re.IGNORECASE)]
+
+    return photos
+
+
+def create_folder_for_renamed_files(path=testing_root_folder):
+    """
+    Creates a folder named "Renamed_Photos" to store the new photos in
+
+    Parameters
+    ----------
+    path: str - A string representation of the root folder for photos where you want the "Renamed_Photos" folder to be
+        created
+
+    Returns
+    -------
+    None - Creates a folder in the designated location
+    """
+    # Create a new folder
+    renamed_folder = Path(path) / 'Renamed_Photos'
+    os.mkdir(renamed_folder)
+
+
+# This function drops illegal characters from file names (like '/', '%', '#' or ',')
+def slugify(value, allow_unicode=False):
+    """
+    Taken from https://github.com/django/django/blob/master/django/utils/text.py
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+
+    Parameters
+    ----------
+    value: str - the original value containing potentially illegal characters
+    allow_unicode: bool - Defaults to False, prevents things like language characters and emojis from being used in
+        file names
+
+    Returns
+    -------
+    str - a string stripped of all illegal characters for use in file names
+    """
+
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
+
+
+def convert_filenames_from_excel(excel_file=testing_excel_file, root_folder=testing_root_folder):
+    """
+    Function that copies a series of file paths from column 'A' of an Excel file (sheet 0, with no header) and changes
+    the file names to the new filename values held in column 'B'. The function uses Django's slugify function to remove
+    illegal or confusing characters like '/', '\', ',', or '#'
+
+    Parameters
+    ----------
+    excel_file: str - Path to Excel file containing file paths and new file names
+    root_folder: str - Path to the folder the Renamed_Photos folder is located
+
+    Returns
+    -------
+    None - creates the files within the new renamed folder
+    """
+    # Get a list of current files and what you want them renamed to
+    photo_list_excel = pd.read_excel(Path(excel_file), 0, header=None)
+    file_list = photo_list_excel[0].tolist()
+    new_names = photo_list_excel[1].tolist()
+
+    # Check for repeat values in the new file name list and append them if they exist
+    duplicates = dict(Counter(new_names))
+    unique_values = unique(new_names)
+    unique_file_names = [slugify(key) if i == 0 else slugify(key + '_' + str(i + 1)) for key in unique_values for i in
+                         range(duplicates[key])]
+
+    # Loop through the Existing files and copy them into the new folder
+    for index, file in enumerate(file_list):
+        old_name = file
+        new_name = Path(root_folder) / "Renamed_Photos" / f"{unique_file_names[index]}.jpg"
+        shutil.copy(old_name, new_name)
+        print(f"Created File: {new_name}")
+
+
+def photo_renaming_tool(root_folder=testing_root_folder, excel_file=None):
+    """
+    Function to simplify the above functions, given a root folder, returns a list of files and
+    searches for an Excel file to use with the renaming tool function
+
+    Parameters
+    ----------
+    root_folder: str - Path to folder containing photos and Excel file defining new names
+    excel_file: str - Path override for if the function can't find the correct file or if it's
+        in a different folder
+
+    Returns
+    -------
+        None - prints list of photo files found and renames them into another folder
+    """
+    full_file_list = get_list_of_files_from_folder(root_folder)
+
+    # Only finds first Excel file, multiple Excel files in the folder will break this
+    if excel_file is None:
+        excel_file = [file for file in full_file_list if re.search("(xlsx|xls|xlsm)$", str(file), re.IGNORECASE)][0]
+
+    photo_list = get_photos_from_file_list(full_file_list)
+    print(photo_list)
+
+    input('Copy the files and names into the excel sheet, then hit "enter"')
+    create_folder_for_renamed_files(root_folder)
+    convert_filenames_from_excel(excel_file=excel_file, root_folder=root_folder)
+
+
+# This allows the file to be run as a script by running `python PhotoOrganizer.py 'path_to_folder'
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        testing_root_folder = Path(sys.argv[1])
+        testing_excel_file = Path(sys.argv[2])
+
+        # These are put here to demonstrate the workflow process and test the file
+        test_files_list = get_list_of_files_from_folder(testing_root_folder)
+        test_photo_list = get_photos_from_file_list(test_files_list)
+        print(test_photo_list)
+
+        input('Copy the files and names into the excel sheet, then hit "enter"')
+        create_folder_for_renamed_files(testing_root_folder)
+        convert_filenames_from_excel(testing_excel_file, testing_root_folder)
+
+    else:
+        testing_root_folder = r"C:\Users\dane\Desktop\test_photos"
+        testing_excel_file = r"C:\Users\dane\Desktop\test_photos\test.xlsx"
+
