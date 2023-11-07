4fa8039193da54d4f84c28548a80a876d080acc6
Dane.Parks <dane@daneparks.com>
2023-11-07 12:12:13 -0500

    Updated Photo renaming tool to give option to include original file name appended to end or not, added training file that only addresses the photo renaming tool.

--- src/civilpy/general/photos.py
+++ src/civilpy/general/photos.py
@@ -182,7 +182,8 @@ def slugify(value, allow_unicode=False):
     return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 
-def convert_filenames_from_excel(excel_file=testing_excel_file, root_folder=testing_root_folder, project_name=''):
+def convert_filenames_from_excel(excel_file=testing_excel_file, root_folder=testing_root_folder, project_name='',
+                                 keep_existing_fns=False):
     """
     Function that copies a series of file paths from column 'A' of an Excel file (sheet 0, with no header) and changes
     the file names to the new filename values held in column 'B'. The function uses Django's slugify function to remove
@@ -193,6 +194,7 @@ def convert_filenames_from_excel(excel_file=testing_excel_file, root_folder=test
     excel_file: str - Path to Excel file containing file paths and new file names
     root_folder: str - Path to the folder the Renamed_Photos folder is located
     project_name: str - Name of the project to be included within the photo names
+    keep_existing_fns: bool - Option to either append the original file names to the new name or not
 
     Returns
     -------
@@ -206,7 +208,10 @@ def convert_filenames_from_excel(excel_file=testing_excel_file, root_folder=test
     # Loop through the Existing files and copy them into the new folder
     for index, name in enumerate(new_names):
         old_name = Path(file_list[index])
-        new_name = Path(root_folder) / "Renamed_Photos" / f"{slugify(name)}-{slugify(old_name.stem)}.jpg"
+        if keep_existing_fns:
+            new_name = Path(root_folder) / "Renamed_Photos" / f"{slugify(name)}-{slugify(old_name.stem)}.jpg"
+        else:
+            new_name = Path(root_folder) / "Renamed_Photos" / f"{slugify(name)}.jpg"
         shutil.copy(old_name, new_name)
         print(f"Created File: {new_name}")
 
